c8339fe0c2862f1769f6b9e153f60d3cae7958d8
Steven Huf <stevehuf@au1.ibm.com>
2012-04-26 15:41:49 +1000

    Anagrams now work. Words with word still is being weird.
    Commmented out error handler.

--- index.php
+++ index.php
@@ -7,16 +7,16 @@ function error_function($level, $message, $file, $line, $context){
 	die(); //Only show an error once (much better than iterating through the whole dictionary and printing 500,000 errors
 }
 
-set_error_handler("error_function");
+//set_error_handler("error_function");
 
 if(empty($_POST['action'])){
 ?>
 <form action="." method="post" target="output" onsubmit="document.getElementById('output').contentDocument.body.innerHTML='Loading...';">
 <label for="word">Word:<input type="text" name="word"/></label><br/>
 <label><input type="radio" name="action" value="in" checked="checked">Find words that contain this word</label><br/>
-<label><input type="radio" name="action" value="rex">Use regular expressions to find words</label><br/>
+<label><input type="radio" name="action" value="rex">Use <a href="http://www.regular-expressions.info/reference.html">regular expressions,</a> to find words</label><br/>
 <label><input type="radio" name="action" value="out">Find words within this word (slow and broken)</label><br/>
-<label><input type="radio" name="action" value="ana">Find anagrams of this word (slow and broken)</label><br/>
+<label><input type="radio" name="action" value="ana">Find anagrams of this word</label><br/>
 <input type="submit"/>
 </form>
 <iframe id="output" name="output" height="100%" width="100%" style="border:none;"/>
@@ -35,51 +35,55 @@ if(empty($_POST['action'])){
 			}
 			break;
 		case('rex'):
+			if($word[0]!="/")
+				$word = "/$word/"; //add regex quotes if they aren't present
 			foreach($file as $test){
 				if(preg_match($word,$test)==1)
 					$words[] = $test;
 			}
 			break;
 		case('ana'):
-		case('out'):/*
+			$anagrams = true;
+		case('out'):
+			$word = str_replace(' ','',strtolower($word));
 			$alpha = 'qwertyuiopasdfghjklzxcvbnm';
-			$testLetters = array();
-			$wordLetters = array();
-			$alphaNo = strlen($alpha);
-			//Set up word array
-			for($i=0;$i<$alphaNo;$i++){
-				$wordLetters[$alpha[$i]] = 0;
+			$alphalen = strlen($alpha);
+			//create blank word array
+			$blank = array();
+			for($i=0;$i!=$alphalen;$i++)
+				$blank[$alpha[$i]] = 0;
+			$wordmap = $blank;
+			$wordlen = strlen($word);
+			for($i=0;$i!=$wordlen;$i++){
+				@$wordmap[$word[$i]]++;
 			}
-			//Add letters to array
-			for($i=0;$i<$alphaNo;$i++){
-				$wordLetters[$alpha[$i]]++;
-			}
-			//Now for the dictionary:
+
+			//Now test dictionary words
+			$testmap = array();
 			foreach($file as $test){
-				//Map the letters in the test word
-				for($i=0;$i<$alphaNo;$i++){
-					$testLetters[$alpha[$i]] = 0;
+				$test = trim(strtolower($test));
+				$testlen = strlen($test);
+				//if not enough letters, then it can't be.
+				if((isset($anagrams) && $testlen != $wordlen) || (!isset($anagrams) && $testlen>$wordlen)) continue;
+				$testmap = $blank;
+				for($i=0;$i!=$wordlen;$i++){
+					if(@isset($testmap[$test[$i]]))
+						$testmap[$test[$i]]++;
+					else
+						continue 2;
 				}
-				for($i=0;$i<$alphaNo;$i++){
-					if(++$testLetters[$alpha[$i]]>$wordLetters[$alpha[$i]]) continue 3; //ignore rest of word
-				}
-				//Test
-				if($_POST['action'] == 'ana'){
-				//Perfectly equal
-					if(strlen($test)==strlen($word)) continue 2; //They're not gunna be the same without the same no of charas
-					for($i=0;$i<$alphaNo;$i++){
-						if($testLetters[$alpha[$i]]!=$wordLetters[$alpha[$i]]) continue 3;
+				
+				if(isset($anagrams)){
+					for($i=0; $i!=$alphalen; $i++){
+						if($testmap[$alpha[$i]] != $wordmap[$alpha[$i]]) continue(2);
 					}
 				} else {
-				//Equal or less
-					for($i=0;$i<$alphaNo;$i++){
-						if($testLetters[$alpha[$i]]<$wordLetters[$alpha[$i]]) continue 3;
+					for($i=0; $i!=$alphalen; $i++){
+						if($testmap[$alpha[$i]] < $wordmap[$alpha[$i]]) continue(2);//flawed
 					}
 				}
 				$words[] = $test;
 			}
-			*/
-			$words[] = "Anagrams are having issues right now...";
 			break;
 		default:
 			$words[] = "A confusion was found.";
